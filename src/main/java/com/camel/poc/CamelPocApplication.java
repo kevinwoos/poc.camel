060d895e2877e1e1f6d0ff68751bafcf60669154
Kevin <ukd210@gmail.com>
2020-02-13 14:31:25 +0900

    init commit

---
+++ src/main/java/com/camel/poc/CamelPocApplication.java
@@ -0,0 +1,18 @@
+package com.camel.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CamelPocApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelPocApplication.class, args);
+	}
+
+
+
+}
